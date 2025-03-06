0021e81ae9d41014df28a13846060748326ffdda
Gabriel Kret <gabrielkret123@gmail.com>
2025-03-06 10:04:36 -0500

    added Mech Pwr vs RPM plots

--- DA_Plotting.py
+++ DA_Plotting.py
@@ -10,6 +10,32 @@ my_path = os.path.dirname(os.path.abspath(__file__))
 # Plot the data
 sns.set()
 #Torque vs RPM for each applied voltage (all plots on a single graph)
+#add plot for mechanical power vs RPM
+for voltage in DA.df['Applied Voltage (v)'].unique():
+    df_v = DA.df[DA.df['Applied Voltage (v)'] == voltage].sort_values(by='Tach Reading (RPM)')
+    fig, ax = plt.subplots()
+    coefficients_RPM_vs_Torque = np.polyfit(df_v['Tach Reading (RPM)'], df_v['Torque (N-m)'], 1)
+    coefficients_Current_vs_RPM = np.polyfit(df_v['Tach Reading (RPM)'], df_v['Current Draw (A)'], 1)
+    stall_torque = coefficients_RPM_vs_Torque[1]
+    print(f"Voltage: {voltage}")
+    print(f"Stall Torque: {stall_torque}")
+    print(f"RPM vs Torque Coefficients: {coefficients_RPM_vs_Torque}")
+    print(f"Current vs RPM Coefficients: {coefficients_Current_vs_RPM}")
+
+    torque_at_rpm = stall_torque + coefficients_RPM_vs_Torque[0] * df_v['Tach Reading (RPM)']
+    current_at_rpm = coefficients_Current_vs_RPM[0] * df_v['Tach Reading (RPM)'] + coefficients_Current_vs_RPM[1]
+    power_output = torque_at_rpm * df_v['Tach Reading (RPM)'] * (2*np.pi/60)
+    power_input = current_at_rpm * voltage
+    eff = power_output / power_input
+    RPM_range = np.linspace(df_v['Tach Reading (RPM)'].min(), df_v['Tach Reading (RPM)'].max(), len(eff))
+    ax.scatter(RPM_range, power_output, label = r"Power Output")
+    #ax.scatter(RPM_range, power_input, label = r"Power Input")
+    plt.legend()
+    ax.set_xlabel('RPM')
+    ax.set_ylabel('Power (W)')
+    plt.title(f'Power vs RPM for {voltage} V')
+    plt.savefig(os.path.join(my_path, f'Plots/Power_vs_RPM/Power_vs_RPM_{voltage}V.png'))
+plt.show()
 
 fig, ax = plt.subplots()
 for voltage in DA.df['Applied Voltage (v)'].unique():
